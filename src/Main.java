70427a5cf82e11f959a9c6017631dd46bbcdddb7
Mates <matej@tsv.cz>
2026-04-27 19:01:02 +0200

    add MainFrame basics

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
+        SpaceObject sun = new SpaceObject(
+                "Sun",
+                false,
+                1.98847e30,
+                new Vector(0, 0),
+                new Vector(0, 0)
+        );
+
+        SpaceObject earth = new SpaceObject(
+                "Earth",
+                false,
+                5.97219e24,
+                new Vector(1.496e11, 0),
+                new Vector(0, 29_780)
+        );
+
+
+        MainFrame frame = new MainFrame();
+        frame.init();
 
     }
+
+
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class MainFrame {
+
+    private JFrame frame;
+
+    public MainFrame() {
+        frame = new JFrame("The Only Place that hasn't been Corrupted by CAPITALISM!");
+    }
+
+    public void init(){
+        this.frame.setSize(1000, 1000);
+        this.frame.setLayout(new BorderLayout());
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setLocationRelativeTo(null);
+        this.frame.setResizable(false);
+
+        this.frame.setVisible(true);
+    }
 }
