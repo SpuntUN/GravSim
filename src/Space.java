1299226075f8f01f6b5d7b6ecd4c375801a3c50a
Mates <matej@tsv.cz>
2026-04-27 16:47:50 +0200

    add calculateForce method to Space

--- src/Space.java
+++ src/Space.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+
 public class Space {
+    private ArrayList<SpaceObject> spaceObjects;
+    private int scale;
+    private final double G = 6.67430e-11;
+
+    public Space() {
+        spaceObjects = new ArrayList<>();
+    }
+
+    public void update(double time){
+        ArrayList<SpaceObject> massBodies = new ArrayList<>();
+        ArrayList<SpaceObject> masslessBodies = new ArrayList<>();
+        for (SpaceObject o : spaceObjects){
+            if (o.isMassless()){
+                masslessBodies.add(o);
+            } else{
+                massBodies.add(o);
+            }
+        }
+
+        for (int i = 0; i < massBodies.size(); i++) {
+            for (int j = i + 1; j < massBodies.size(); j++) {
+
+            }
+        }
+
+    }
+
+    private Vector calculateForce(SpaceObject o1, SpaceObject o2){
+        Vector r = Vector.subtract(o1.getPosition(), o2.getPosition());
+        double distance = Vector.normalize(r);
+        double m1 = o1.getMass();
+        double m2 = o2.getMass();
+
+        return Vector.negate(Vector.multiply(r, G*m1*m2/Math.pow(distance, 3)));
+    }
+
 }
