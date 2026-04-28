407156db6ddf3820a3fbb931288fc9c1785013eb
Mates <matej@tsv.cz>
2026-04-28 08:12:10 +0200

    fix Radius not being allocated

--- src/SpacePanel.java
+++ src/SpacePanel.java
@@ -4,11 +4,11 @@ import java.awt.*;
 public class SpacePanel extends JPanel{
 
     private Space space;
+    private double scale = 1;
 
     public SpacePanel(Space space) {
         this.space = space;
-        this.setLayout(new BorderLayout());
-        this.setBackground(Color.BLACK);
+        init();
     }
 
     @Override
@@ -27,7 +27,27 @@ public class SpacePanel extends JPanel{
     private void paintSpaceObjects(Graphics2D g2d){
         for (SpaceObject o : space.getSpaceObjects()){
             g2d.setColor(o.getColor());
-            g2d.fillOval((int) o.getPosition().getX(), (int) o.getPosition().getX(), (int) o.getRadius(), (int) o.getRadius());
+            g2d.fillOval((int) (o.getPosition().getX()/scale), (int) (o.getPosition().getY()/scale), (int) (o.getRadius()/scale), (int) (o.getRadius()/scale));
+            if (o.getName().equals("Sun")){
+                System.out.println(o.getRadius());
+                System.out.println(o.getRadius()/scale);
+            }
         }
     }
+
+
+    private void init(){
+        this.setLayout(new BorderLayout());
+        this.setBackground(Color.BLACK);
+
+        this.addMouseWheelListener(e -> {
+            int rotation = e.getWheelRotation();
+            if (rotation < 0) {
+                scale /= 1.1;
+            } else {
+               scale *= 1.1;
+            }
+        });
+    }
+
 }
